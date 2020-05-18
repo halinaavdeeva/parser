aa1e85b38d25a993a43c60a9391aa888888d080d
Halina_Avdeeva <halina_avdeeva@epam.com>
2020-05-18 09:05:42 +0300

    update

--- src/Main.py
+++ src/Main.py
@@ -8,8 +8,7 @@ def main():
     folder.create_folder()
     folder.move_file()
     for file in folder.find_all_fb2_books(folder.path_input):
-        if file.endswith(".fb2"):
-            Connection().for_main(Model(folder.path_input, file))
+        Connection().for_main(Model(folder.path_input, file))
 
 
 if __name__ == "__main__":
--- src/file_work/file_handler.py
+++ src/file_work/file_handler.py
@@ -12,31 +12,25 @@ class FileHandler:
         self.path_incorrect_input = ""
 
     def create_folder(self):
-        home = os.path.expanduser('~')
-        self.path_input = os.path.join(home, 'Documents', 'books', 'input')
-        self.path_incorrect_input = os.path.join(home, 'Documents', 'books', 'incorrect_input')
+        page = os.path.expanduser('~')
+        self.path_input = os.path.join(page, 'Desktop', 'input')
+        self.path_incorrect_input = os.path.join(page, 'Desktop', 'incorrect_input')
 
         try:
-            if not os.path.isdir(os.path.join(home, 'Documents', 'books')):
-                os.mkdir(os.path.join(home, 'Documents', 'books'))
             if not os.path.isdir(self.path_input):
-                os.mkdir(os.path.join(home, 'Documents', 'books', 'input'))
-                logging.info("Input folder was created")
+                os.mkdir(os.path.join(page, 'Desktop', 'input'))
             if not os.path.isdir(self.path_incorrect_input):
                 os.mkdir(self.path_incorrect_input)
+                logging.info("Input and Incorrect input folders were created")
         except OSError as error:
-            logging.error("Creation of the directory failed")
+            logging.error("Creation of the folder failed", error)
 
     def move_file(self):
         for file in os.listdir(self.path_input):
             if not file.endswith(".fb2"):
                 logging.info("File {} was moved to {}".format(os.path.join(self.path_input,
                                                                            file), self.path_incorrect_input))
-                try:
-                    shutil.move(os.path.join(self.path_input, file), self.path_incorrect_input)
-                except:
-                    logging.error(
-                        "File {} already exists in directory {}".format(file, self.path_incorrect_input))
+                shutil.move(os.path.join(self.path_input, file), self.path_incorrect_input)
 
     def find_all_fb2_books(self, path):
         list_of_files = []
